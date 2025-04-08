09845715e4b7ec1a32d0cd3bf7f9a36eef7924c2
vkatsel <vkatsel@kse.org.ua>
2025-04-08 23:19:20 +0300

    Implemented BFS

---
+++ dm_project/dm_project/Bfs.cs
@@ -0,0 +1,54 @@
+﻿using dm_project.DataStructures;
+
+namespace dm_project;
+
+public static class Bfs
+{
+    public static List<Vertex> Breadth_First_Search(Vertex a, Vertex b)
+    {
+        var queue = new Queue<Vertex>();
+        
+        var visited = new HashSet<Vertex>();
+        var parent = new Dictionary<Vertex, Vertex>();
+        var distance = new Dictionary<Vertex, int>();
+        
+        visited.Add(a);
+        queue.Enqueue(a);
+        distance[a] = 0;
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+            if (b.Equals(current))
+            {
+                return GetPath(a, current, parent);
+            }
+                
+            foreach (var adjacentVertex in current.AdjacentVertices)
+            {
+                if (!visited.Contains(adjacentVertex))
+                {
+                    parent[adjacentVertex] = current;
+                    distance[adjacentVertex] = distance[current] + 1;
+                    visited.Add(adjacentVertex);
+                    queue.Enqueue(adjacentVertex);
+                }
+            }
+        }
+
+        return new List<Vertex>();
+    }
+
+    private static List<Vertex> GetPath(Vertex a, Vertex current, Dictionary<Vertex, Vertex> parent)
+    {
+        var path = new List<Vertex>();
+        while (!current.Equals(a))
+        {
+            path.Add(current);
+            current = parent[current];
+        }
+        path.Add(current);
+        path.Reverse();
+        return path;
+    }
+}
--- dm_project/dm_project/DataStructures/Vertex.cs
+++ dm_project/dm_project/DataStructures/Vertex.cs
@@ -4,4 +4,12 @@ public class Vertex(string name)
 {
     public string Name { get; set; } = name;
     public HashSet<Vertex> AdjacentVertices { get; } = new HashSet<Vertex>();
+    
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Vertex other)
+            return false;
+
+        return Name.Equals(other.Name) && AdjacentVertices.SetEquals(AdjacentVertices);
+    }
 }
