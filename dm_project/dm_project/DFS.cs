8b14cb276354816b0ac70d2610d0138326f0365a
Emma Nor <enor@kse.org.ua>
2025-04-11 12:07:36 +0300

    add DFS

---
+++ dm_project/dm_project/DFS.cs
@@ -0,0 +1,56 @@
+﻿using dm_project.DataStructures;
+
+namespace dm_project;
+
+public static class DFS
+{
+    public static (List<Vertex>, bool) Depth_First_Search(Vertex a, Vertex b)
+    {
+        var visited = new HashSet<Vertex>();
+        var parent = new Dictionary<Vertex, Vertex>();
+
+        bool found = DFS_Helper(a, b, visited, parent);
+
+        if (!found)
+            return (new List<Vertex>(), false);
+
+        return (GetPath(a, b, parent), true);
+    }
+
+    private static bool DFS_Helper(Vertex current, Vertex target, HashSet<Vertex> visited, Dictionary<Vertex, Vertex> parent)
+    {
+        visited.Add(current);
+
+        if (current.Equals(target))
+            return true;
+
+        foreach (var neighbor in current.AdjacentVertices)
+        {
+            if (!visited.Contains(neighbor))
+            {
+                parent[neighbor] = current;
+
+                if (DFS_Helper(neighbor, target, visited, parent))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static List<Vertex> GetPath(Vertex start, Vertex end, Dictionary<Vertex, Vertex> parent)
+    {
+        var path = new List<Vertex>();
+        var current = end;
+
+        while (!current.Equals(start))
+        {
+            path.Add(current);
+            current = parent[current];
+        }
+
+        path.Add(start);
+        path.Reverse();
+        return path;
+    }
+}
